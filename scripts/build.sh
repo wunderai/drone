dcf0f97d4afd2c97d236db0a8b4e82ab581fcf01
Goesta von Stebut <gvs@s42.net>
2021-03-23 12:16:15 +0000

    enable nolimit build

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ echo "building docker images for ${GOOS}/${GOARCH} ..."
 REPO="github.com/drone/drone"
 
 # compile the server using the cgo
-go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
+go build -tags "nolimit" -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
